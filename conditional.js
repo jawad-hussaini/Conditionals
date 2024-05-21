3874931d6fde20771e19afaff4e7adfbfd29cf80
Syed Jawad <jawadhussaini1804@gmail.com>
2024-05-21 09:53:55 +0500

    condition

---
+++ conditional.js
@@ -0,0 +1,15 @@
+// let a = ["ali", "fatima"];
+// let b = ["aliya", "fatim"];
+// if(a.length === b.length){
+//     console.log("These two variables has equal length")
+// }
+// else{
+//     console.log("These variables hasn't equal length")
+// }
+
+let age = 2;
+if(age >= 18){
+    console.log("You can")
+}else{
+    console.log("You can't")
+}
